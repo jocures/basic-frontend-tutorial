9e3a8f0fedc91c7a5b2e80b2ee54d1b9cb86ab6b
Jeremy Neal <jneal1992@gmail.com>
2017-10-12 16:40:22 -0400

    Toggling styles with JavaScript.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This project is a tutorial on front-end development basics using HTML, CSS and
 JavaScript (all ES6).
 
-We're going to build a basic to-do app. Let's add some styling to our list using
-CSS.
+We're going to build a basic to-do app. Let's add some JavaScript so we can
+programatically change what the todo items look like.
---
+++ todoapp.js
@@ -0,0 +1,11 @@
+(() => {
+  let todos = [...document.querySelectorAll("div.todo-item")];
+  todos.map((todo) => {
+    todo.addEventListener("click", () => {
+      todo.classList.toggle("completed");
+      var [checkbox, text] = todo.children;
+      checkbox.checked = !checkbox.checked;
+      text.classList.toggle("completed", todo.classList.contains("completed"));
+    })
+  });
+})();
